6b3b19adac83fa3067f3a81dc1ec13ee0a556bae
Victor Campos <vcampos@lafabrica.local>
2021-08-16 09:20:25 -0400

    mensajes de error y validaciones

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors"
 import router from "./routes"
 import log from "./logger"
 import connect from "./db"
+import custom404Handler from "./utils/customErrorsPages"
 
 config()
 
@@ -13,6 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use( cors() )
 
 app.use("/api", router)
+
+custom404Handler(app)
 
 app.listen(process.env.PORT, () =>{
     log.info(`App listening on port ${ process.env.PORT }`)
